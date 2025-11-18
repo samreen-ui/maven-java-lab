a4578c01de382c586a2587be636ac2117a5f027f
samreen-ui <samreenf3112@gmail.com>
2025-11-18 10:45:25 +0530

    first commit

---
+++ src/main/java/com/kmit/my_java_app/App.java
@@ -0,0 +1,14 @@
+package com.kmit.my_java_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    } 
+}
+
